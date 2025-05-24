6a64a0aac29f0ee64203e6c554185a3ebabbf3db
Abel González <86550381+Copipan@users.noreply.github.com>
2025-05-23 19:08:04 -0600

    Se crea conexión con la base de datos
    - Se necesita tener la base de datos ya creada en el sistema, así como también el tener las dependencias instaladas de python

--- yosirvo-app/src/backend/BaseDeDatos.py
+++ yosirvo-app/src/backend/BaseDeDatos.py
@@ -1,4 +1,4 @@
 import cx_Oracle
 
 def get_connection():
-    return cx_Oracle.connect("usuario", "contraseña", "host:")
+    return cx_Oracle.connect("yosirvo", "yosirvo", "localhost:1521/xepdb1")
