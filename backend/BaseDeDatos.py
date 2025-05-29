f7e694ed002e78243df90ddf67b3c894d522fa0b
Miguel R. <a343786@uach.mx>
2025-05-28 22:40:49 -0600

    Se actualiza BaseDeDatos.py y se agrega un ERwin de la base de datos

--- backend/BaseDeDatos.py
+++ backend/BaseDeDatos.py
@@ -1,4 +1,7 @@
 import cx_Oracle
 
+#No la borren pls, es para que la pueda correr yo (el maik xdlol)
+#cx_Oracle.init_oracle_client(lib_dir=r"C:\OracleXE\instantclient_23_8")
+
 def get_connection():
     return cx_Oracle.connect("yosirvo", "yosirvo", "localhost:1521/xepdb1")
