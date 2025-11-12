69f2764f0b9a062fd0ee81ca896fd9f69cfb93be
MoXcz <130593875+MoXcz@users.noreply.github.com>
2025-11-12 14:49:46 -0600

    feat: nuevo driver para Oracle

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,17 @@
 import cx_Oracle
 
-#No la borren pls, es para que la pueda correr yo (el maik xdlol)
-#cx_Oracle.init_oracle_client(lib_dir=r"C:\OracleXE\instantclient_23_8")
+# import oracledb
+
+# No la borren pls, es para que la pueda correr yo (el maik xdlol)
+# cx_Oracle.init_oracle_client(lib_dir=r"C:\OracleXE\instantclient_23_8")
+
 
 def get_connection():
     return cx_Oracle.connect("yosirvo", "yosirvo", "localhost:1521/xepdb1")
+
+
+# Oscar: Ocupe usar driver para hacerlo funcionar
+# def get_connection():
+#     return oracledb.connect(
+#         user="yosirvo", password="yosirvo", dsn="localhost:1521/xepdb1"
+#     )
